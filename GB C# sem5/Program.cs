80275911f70998cf9bb2024bb1d03efd22d071f0
DudeBash <70896241+DudeBash@users.noreply.github.com>
2023-02-10 02:49:45 +0300

    done

---
+++ GB C# sem5/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Задайте коэфецент b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Задайте коэфецент k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Задайте коэфецент b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Задайте коэфецент k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x = 0;
+double y = 0;
+if ((k1 == k2)&&(b1==b2))
+{
+    Console.WriteLine("прямые паралельны");
+}
+else
+{
+x = (b2-b1)/(k1-k2);
+y = (k1*(b2-b1))/(k1-k2)+b1;
+Console.WriteLine("Точка пересечения " + x + " " + y );
+}
