22bd8d8a88539cfa5cd339201d7e3eaefd65aefa
DudeBash <70896241+DudeBash@users.noreply.github.com>
2023-02-14 00:18:47 +0300

    done

---
+++ GB C# sem 5/Program.cs
@@ -0,0 +1,37 @@
+﻿//Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+Console.WriteLine("Задайте коэфецент b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Задайте коэфецент k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Задайте коэфецент b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Задайте коэфецент k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x = 0;
+double y = 0;
+if ((k1 == k2)&&(b1==b2))
+{
+    Console.WriteLine("прямые паралельны");
+}
+else
+{
+x = (b2-b1)/(k1-k2);
+y = (k1*(b2-b1))/(k1-k2)+b1;
+Console.WriteLine("Точка пересечения " + x + " " + y );
+}
+//Написать программу масштабирования фигуры
+int side1 = new Random().Next(1,10);
+int side2 = new Random().Next(1,10);
+int side3 = new Random().Next(1,10);
+
+if ((side1 + side2>side3)&&(side2+side3>side1)&&(side3+side1>side2)){
+    Console.WriteLine("сгенерирован треугольник со стронми" );
+    Console.WriteLine("строна1: " + side1);
+    Console.WriteLine("строна2: " + side2);
+    Console.WriteLine("строна3: " + side3);
+}
+else{
+    side1 = new Random().Next(1,10);
+    side2 = new Random().Next(1,10);
+    side3 = new Random().Next(1,10);
+}
---
+++ GB C# sem 6/Program.cs
@@ -0,0 +1,67 @@
+﻿//Показать двумерный массив размером m×n заполненный вещественными числами
+Console.WriteLine("введите количество столбцов");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("введите высоту столбцов");
+int y = int.Parse(Console.ReadLine());
+int [,] arr = new int [x,y];
+
+void fillArr (int [,] arr)
+{
+    for (int fill_x=0; fill_x < arr.GetLength(0); fill_x++)
+    {
+       for (int fill_y=0; fill_y < arr.GetLength(1);fill_y++)
+       {
+         arr[fill_x,fill_y] = new Random().Next(1,999);
+       }
+       
+    }
+    
+}
+
+
+void OutputArr (int [,] arr)
+{
+    for (int fill_x=0; fill_x < arr.GetLength(0); fill_x++)
+    {
+       for (int fill_y=0; fill_y < arr.GetLength(1);fill_y++)
+       {
+         Console.WriteLine(arr[fill_x,fill_y]);
+       }
+       
+    }
+    
+}
+
+//Задать двумерный массив следующим правилом: Aₘₙ = m+n (не смог понять что нужно сделать)
+
+//В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+for (int fill_x = 0; fill_x < arr.GetLength(0);fill_x++)
+{
+    for (int fill_y = 0; fill_y < arr.GetLength(1);fill_y++)
+    {
+        if (((fill_y%2)==0)&&((fill_x%2)==0))
+        {
+            arr [fill_x,fill_y] = arr [fill_x,fill_y]*arr [fill_x,fill_y];
+        }
+    }
+}
+fillArr(arr);
+OutputArr(arr);
+int sum = 0;
+//В матрице чисел найти сумму элементов главной диагонали
+for (int fill_x = 0; fill_x < arr.GetLength(0);fill_x++)
+{
+    for (int fill_y = 0; fill_y < arr.GetLength(1);fill_y++)
+    {
+        if (((fill_y%2)==0)&&((fill_x%2)==0))
+        {
+            if (fill_x == fill_y)
+            {
+                sum += arr[fill_x,fill_y];
+            }
+        }
+    }
+}
+
+Console.WriteLine("сумму элементов главной диагонали " + sum );
+
